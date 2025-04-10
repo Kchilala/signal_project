50818c28fb4a89f12c8f03dabbd854efa1f0b53b
lars <lars.van.dijnen@gmail.com>
2025-04-10 15:15:50 +0200

    lars

--- src/main/java/com/cardio_generator/outputs/FileOutputStrategy.java
+++ src/main/java/com/cardio_generator/outputs/FileOutputStrategy.java
@@ -24,18 +24,18 @@ public class FileOutputStrategy implements OutputStrategy {
         // Create the directory
         Files.createDirectories(Paths.get(baseDirectory));
       } catch (IOException e) {
-          System.err.println("Error creating base directory: " + e.getMessage());
-          return;
+        System.err.println("Error creating base directory: " + e.getMessage());
+        return;
       }
         // Set the FilePath variable
       String filePath = fileMap.computeIfAbsent(label, k -> Paths.get(baseDirectory, label + ".txt").toString());
 
         // Write the data to the file
       try (PrintWriter out = new PrintWriter(
-               Files.newBufferedWriter(Paths.get(filePath), StandardOpenOption.CREATE, StandardOpenOption.APPEND))) {
-                 out.printf("Patient ID: %d, Timestamp: %d, Label: %s, Data: %s%n", patientId, timestamp, label, data);
+          Files.newBufferedWriter(Paths.get(filePath), StandardOpenOption.CREATE, StandardOpenOption.APPEND))) {
+            out.printf("Patient ID: %d, Timestamp: %d, Label: %s, Data: %s%n", patientId, timestamp, label, data);
       } catch (Exception e) {
-          System.err.println("Error writing to file " + filePath + ": " + e.getMessage());
+        System.err.println("Error writing to file " + filePath + ": " + e.getMessage());
       }
     }
 }
