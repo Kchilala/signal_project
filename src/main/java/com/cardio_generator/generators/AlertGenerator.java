66e4df38dbbfe958e6b78aff6c6a2ab38ac8c394
lars <lars.van.dijnen@gmail.com>
2025-04-11 10:20:24 +0200

    javadoc

--- src/main/java/com/cardio_generator/generators/AlertGenerator.java
+++ src/main/java/com/cardio_generator/generators/AlertGenerator.java
@@ -4,17 +4,41 @@ import java.util.Random;
 
 import com.cardio_generator.outputs.OutputStrategy;
 
-
+/**
+ * Generates simulated alert data for patients.
+ * <p>
+ * This class simulates the occurrence of alerts for patients. The alerts can either be triggered or resolved,
+ * and the state of the alerts is updated and sent to the specified output strategy.
+ * </p>
+ */
 public class AlertGenerator implements PatientDataGenerator {
 
   public static final Random randomGenerator = new Random();
   
   private boolean[] alertStates; // false = resolved, true = pressed
   // change method lower camel case
+  /**
+   * Constructs an AlertGenerator for a specified number of patients.
+   * <p>
+   * Initializes the alert states for each patient to false (resolved).
+   * </p>
+   * 
+   * @param patientCount The number of patients to generate alert data for.
+   */
   public AlertGenerator(int patientCount) { 
     alertStates = new boolean[patientCount + 1];
   }
 
+  /**
+   * Generates alert data for a specific patient and sends it to the provided output strategy.
+   * <p>
+   * The alert can either be resolved with a 90% probability if it was previously triggered,
+   * or it can be triggered based on a Poisson distribution with a specified rate (Lambda).
+   * </p>
+   * 
+   * @param patientId The unique identifier of the patient.
+   * @param outputStrategy The strategy used to output the generated data (e.g., console, file, etc.).
+   */
   @Override
   public void generate(int patientId, OutputStrategy outputStrategy) {
     try {
